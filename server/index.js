96ec385d4432f3d62fb89dd99de65078c1db4a63
lietuviskalba <mantasling@gmail.com>
2025-04-01 21:47:47 +0300

    ft: add track web app inactivity for server

--- server/index.js
+++ server/index.js
@@ -1,8 +1,22 @@
 // server/index.js
 const express = require("express");
 const path = require("path");
+const fs = require("fs"); // Added to update the activity file
 const app = express();
 const port = process.env.PORT || 3000;
+
+// Middleware to record activity
+// This middleware updates the /tmp/last_web_activity file on every HTTP request.
+// An external script can use this file's modification time to determine inactivity.
+app.use((req, res, next) => {
+  const activityFile = "/tmp/last_web_activity";
+  fs.writeFile(activityFile, Date.now().toString(), (err) => {
+    if (err) {
+      console.error("Failed to update activity file:", err);
+    }
+    next();
+  });
+});
 
 // Serve static files from the React app build folder
 app.use(express.static(path.join(__dirname, "../client/build")));
